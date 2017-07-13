e9e3fd3cfad5cbdc7e0b20a1440953e4386aea6b
Gosuke Miyashita <gosukenator@gmail.com>
2017-07-13 14:09:19 +0900

    Fix typo

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,7 +6,7 @@ use specinfra::backend::Backend;
 fn main() {
     let b = backend::direct::Direct::new();
     let s = specinfra::new(&b).unwrap();
-    let file = s.file("/etc/paasswd");
+    let file = s.file("/etc/passwd");
     match file.mode() {
         Ok(m) => println!("{:o}", m),
         Err(e) => println!("{}", e),
