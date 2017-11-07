3d0c0e873c8df53f82ce9f71090028d7a8e53990
Gosuke Miyashita <gosukenator@gmail.com>
2017-11-08 01:39:43 +0900

    Add Ruby examples using SSH backend

--- ruby/ssh_backend_with_port.rb
+++ ruby/ssh_backend_with_port.rb
@@ -1,7 +1,14 @@
 require 'libspecinfra'
 require 'libspecinfra/backend/ssh'
 
-b = Libspecinfra::Backend::SSH::Binding.new("localhost")
+b = Libspecinfra::Backend::SSH::Binding.new(
+  "localhost",
+  "mizzy",
+  {
+    port: 22,
+  },  
+)
+
 s = Libspecinfra::Specinfra::Binding.new(b)
 f = s.file("/etc/passwd")
 
